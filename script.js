8b0d17f8f02d139e3b059070110bc11485b0ebcb
cassidysisler <cassidysisler@Cassidys-MacBook-Air.local>
2017-04-20 15:58:30 -0500

    Test

---
+++ script.js
@@ -0,0 +1,13 @@
+/*
+    Creator: Cassidy Sisler
+    Date: April 18, 2017
+    Name of File: scripts.js
+    Description: Learning Operators, Comparisons, and Equalities
+*/
+
+var a, b;
+a = "5";
+b = 5;
+
+console.log(a==b);
+console.log(a===b);
